1eb94a7e245164ee2cd9f60b8e0e6f9266c04f20
Unknown <10793160@my.uvu.edu>
2018-01-25 19:26:19 -0700

    Added Player Controller

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
 
-    public float transX;
-    public float transY;
-    public float transZ;
+    // public float transX;
+    // public float transY;
+    // public float transZ;
 
-	public float rotX;
-	public float rotY;
-	public float rotZ;
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
 
-
+		public Vector3 move;
+		public Vector3 rotate;
+		public float  moveSpeed;
 
 
 	// Use this for initialization
@@ -22,7 +24,26 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(transX, transY, transZ);
-		transform.Rotate(rotX, rotY, rotZ);
+	// 	transform.Translate(transX, transY, transZ);
+	// 	transform.Rotate(rotX, rotY, rotZ);
+	if(Input.GetKey(KeyCode.W)){
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
 	}
+
+	if(Input.GetKey(KeyCode.S)){
+		transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+	}
+
+	if(Input.GetKey(KeyCode.Q)){
+		transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+	}
+
+	if(Input.GetKey(KeyCode.E)){
+		transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+	}
+	// transform.Translate(move * Time.deltaTime);
+	// transform.Rotate(rotate * Time.deltaTime);
+
+}
 }
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	public float jumpHeight;
+
+
+	void Update () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+		
+	}
+}
