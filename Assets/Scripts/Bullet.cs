bbdaf6e09128ce6c307a9917df6051325b4d0a37
Unknown <10793160@my.uvu.edu>
2018-02-01 19:57:23 -0700

    Added Game Objects shooting element etc.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public int damage = 1;
+	public int time = 5;
+
+void Start(){
+	StartCoroutine(DestroyBullet());
+}
+	
+IEnumerator DestroyBullet(){
+	yield return new WaitForSeconds(time);
+	Destroy(gameObject);
+}
+
+}
