0d0c9cd0989fd349d309f9da4df6d81a34fd1d1b
Unknown <10793160@my.uvu.edu>
2018-02-08 18:40:07 -0700

    Added chicken Ai

---
+++ Assets/Scripts/ChickenAi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAi : MonoBehaviour {
+	public float moveSpeed;
+	public Transform target;
+	public Transform chickenPen;
+
+	public int points = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void onTriggerStay(Collider other)
+	{
+
+		if(other.gameObject.name == "Player"){
+				Debug.Log("Player has entered chickens trigger");
+				transform.LookAt(target);
+				transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.name == "Player"){
+
+		// scoreManager.AddPoints(points);
+
+		transform.position = chickenPen.position;
+		transform.rotation = chickenPen.rotation;
+		}
+	}
+
+}
+
